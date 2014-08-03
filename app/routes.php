164c03a62ce5300672145e410ebfac721d5a41f0
eod696 <eod696@gmail.com>
2014-08-03 06:58:24 +0000

    require auth to edit users

--- app/routes.php
+++ app/routes.php
@@ -15,8 +15,17 @@ Route::get('/', ['as' => 'index', 'uses' => 'HomeController@showWelcome']);
 
 Route::get('login', ['as' => 'loginForm', 'uses' => 'LoginController@loginForm']);
 Route::post('login', ['as' => 'loginPost', 'uses' => 'LoginController@loginPost']);
+Route::get('logout', ['as' => 'logout', 'uses' => 'LoginController@logout']);
 
 Route::get('users', ['as' => 'getUserIndex', 'uses' => 'UsersController@userIndex']);
 Route::get('users/{id}', ['as' => 'getUser', 'uses' => 'UsersController@showProfile']);
-Route::get('users/{id}/edit', ['before' => 'auth', 'as' => 'editUser', 'uses' => 'UsersController@editUser']);
-Route::post('users/{id}/edit', ['as' => 'updateUser', 'uses' => 'UsersController@editUserPost']);
+Route::get('users/{id}/edit', [
+	'before' => 'auth', 
+	'as' => 'editUser', 
+	'uses' => 'UsersController@editUser'
+]);
+Route::post('users/{id}/edit', [
+	'before' => 'auth', 
+	'as' => 'updateUser', 
+	'uses' => 'UsersController@editUserPost'
+]);
