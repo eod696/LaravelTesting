ad398000728e1dd6ab4b99141ee23ef740ee690b
eod696 <eod696@gmail.com>
2014-08-07 15:57:26 +0000

    added handler for session flash messages on login form

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -6,6 +6,9 @@
 
 @section('content')
 	<h3>Login Form</h3>
+	@if(Session::has('msg'))
+		<div class="message">{{ Session::get('msg') }}</div>
+	@endif
 	{{ Form::open(['route' => 'loginPost', 'class' => 'loginForm']) }}
 		{{ Form::label('email', 'Email:') }}
 		{{ Form::text('email', 'your@email.com') }}
