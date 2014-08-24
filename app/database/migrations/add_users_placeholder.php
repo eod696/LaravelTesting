88a3b3e62ab4327e76e7e0ce456304cf72cca659
Daniel <eod696@gmail.com>
2014-08-24 14:30:20 -0400

    added placeholder migration for add_users migration (became obsolete when seeder method was introduced - this is just a fix for migrations in case it's necessary)

---
+++ app/database/migrations/add_users_placeholder.php
@@ -0,0 +1,14 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUsers extends Migration {
+	public function up() {
+		// placeholder
+	}
+	
+	public function down(){
+		//placeholder
+	}
+}
+?>
